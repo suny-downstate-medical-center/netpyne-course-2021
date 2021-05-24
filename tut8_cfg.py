c8bda953979118427afde524702a35a17d5035dc
Joe Graham <joe.w.graham@gmail.com>
2021-05-24 02:45:12 -0400

    Added another trace and cell to record, added cfg.synMechTau1

--- tut8_cfg.py
+++ tut8_cfg.py
@@ -6,16 +6,18 @@ cfg = specs.SimConfig()     # object of class SimConfig to store simulation conf
 cfg.duration = 1*1e3        # Duration of the simulation, in ms
 cfg.dt = 0.025              # Internal integration timestep to use
 cfg.verbose = False         # Show detailed messages
-cfg.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict with traces to record
+cfg.recordTraces = {'V_soma': {'sec':'soma', 'loc':0.5, 'var': 'v'}}  
+cfg.recordTraces['iNa_soma'] = {'sec':'soma', 'loc':0.5, 'mech':'hh', 'var':'ina'}
 cfg.recordStep = 0.1        # Step size in ms to save data (eg. V traces, LFP, etc)
 cfg.filename = 'tut8'       # Set file output name
 cfg.saveJson = True
 cfg.printPopAvgRates = True
 cfg.analysis['plotRaster'] = {'saveFig': True}                   # Plot a raster
-cfg.analysis['plotTraces'] = {'include': [0], 'saveFig': True}  # Plot recorded traces for this list of cells
+cfg.analysis['plotTraces'] = {'include': [('S',0), ('M',0)], 'saveFig': True}  # Plot recorded traces for this list of cells
 
 cfg.saveDataInclude = ['simData', 'simConfig', 'netParams', 'net']
 
 # Variable parameters (used in netParams)
+cfg.synMechTau1 = 0.1
 cfg.synMechTau2 = 5
 cfg.connWeight = 0.01
